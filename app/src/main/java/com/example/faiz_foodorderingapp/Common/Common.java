b352f90d759e42e3bc38a9d7ca9fc34ecf1d57cf
iamfaizuddin <40165312+iamfaizuddin@users.noreply.github.com>
2019-05-22 15:17:58 +0530

    For Updating Order

--- app/src/main/java/com/example/faiz_foodorderingapp/Common/Common.java
+++ app/src/main/java/com/example/faiz_foodorderingapp/Common/Common.java
@@ -1,8 +1,10 @@
 package com.example.faiz_foodorderingapp.Common;
 
+import com.example.faiz_foodorderingapp.Model.Order;
 import com.example.faiz_foodorderingapp.Model.User;
 
 public class Common {
     public static User currentUser;
-    public static final String DELETE = "Delete";
+    public static Integer update = 0;
+    public static Order orderdetails;
 }
